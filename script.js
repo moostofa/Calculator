91a33d785ca67b5dd2a91a88dee0771946b54b89
moostofa <mmdrahinz96@gmail.com>
2021-11-19 10:41:09 +1100

    DEL, AC, ., and ANS operators are functional (ANS has a bug)

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// the operations that will be performed on previousValue and currentValue when user clicks "="
 const OPERATIONS = {
     "arithmetic": {
         "x": (a, b) => a * b, 
@@ -10,7 +9,7 @@ const OPERATIONS = {
     "visual": {
         "DEL": () => currentValue = currentValue.slice(0, -1), 
         "AC": () => currentValue = "",
-        ".": () => currentValue += ".",  // incomplete
+        ".": () => currentValue += ".", 
         "10^x": () => 10 ** currentValue, 
         "ANS": () => currentValue = previousValue, 
     },
@@ -28,9 +27,7 @@ let currentValue = ""
 let operationToPerform = ""
 
 // display the calculator on the screen after DOM is loaded
-document.addEventListener("DOMContentLoaded", () => {
-    calculator()
-})
+document.addEventListener("DOMContentLoaded", () => calculator())
 
 // calculator constructor
 function calculator() {
@@ -49,7 +46,7 @@ function calculator() {
         const row = document.createElement("div")
         row.classList = "row"
 
-        // -- key -- is "numbers" or "operators" and -- value -- is an array (.forEach() can be used)
+        // -- key -- will be "numbers" or "operators" and -- value -- is an array of values for the buttons
         Object.entries(btnRow).forEach(([key, value]) => {
             value.forEach(val => {
                 const btn = document.createElement("button")
@@ -66,32 +63,33 @@ function calculator() {
     })
 }
 
-// 1. If a number button is clicked, append the currentValue string with that number
-// 2. If an operator button is clicked, validate the button value and save it as the operation to be performed
+// 1. If a number button was clicked, add its value to the currentValue string
+// 2. Else, an operator button was clicked - perform either an arithmetic or visual operation
 function calculate(btnValue) {
-    // incase user inspects element and changes the value of a button
-    if (!OPERATIONS.operators().includes(btnValue))
-    return alert("Invalid button - Button value provided does not match its expected value")
-
-    // 1. If a number button was clicked, add its value to the currentValue string
-    // 2. Else, an operator button was clicked - perform either an arithmetic or visual operation
     if (!isNaN(parseFloat(btnValue))) {
         currentValue += btnValue
     } else {
-        // if either the previous or current value are empty, another operand is required
-        // else, a calculation can be performed with the two operands
-        if (previousValue.length === 0 || currentValue.length === 0) {
-            operationToPerform = btnValue
-            previousValue = currentValue
-            currentValue = ""
-        } else {
-            const result = OPERATIONS["arithmetic"]["="]()
-            currentValue = (btnValue === "=") ? result : ""
-            previousValue = (btnValue === "=") ? "" : result
-            operationToPerform = btnValue
+        // error checking 
+        if (!OPERATIONS.operators().includes(btnValue))
+            return alert("Invalid button - Button value provided does not match its expected value")
+
+        if (Object.keys(OPERATIONS["arithmetic"]).includes(btnValue)) {
+            // if either the previous or current value are empty, another operand is required
+            // else, a calculation can be performed with the two operands
+            if (previousValue.length === 0 || currentValue.length === 0) {
+                operationToPerform = btnValue
+                previousValue = currentValue
+                currentValue = ""
+            } else {
+                const result = OPERATIONS["arithmetic"]["="]()
+                currentValue = (btnValue === "=") ? result : ""
+                previousValue = (btnValue === "=") ? "" : result
+                operationToPerform = btnValue
+            }
+        } else {    // visual operation
+            OPERATIONS["visual"][btnValue]()
         }
     }
-
     document.getElementById("previous-calculation").innerHTML = `${previousValue} ${operationToPerform} ${currentValue}`
     document.getElementById("current-operator").innerHTML = operationToPerform
     document.getElementById("current-calculation").innerHTML = currentValue
